26210a6cea4e500c9a8c991d1293fe40c445fbc9
JigmeT12 <jigmetashi02@gmail.com>
2021-03-30 03:55:17 +0600

    ADDED: discount features with the number of shirts bought

--- app/models/list_item.rb
+++ app/models/list_item.rb
@@ -1,4 +1,17 @@
 class ListItem < ApplicationRecord
   belongs_to :cart
   belongs_to :clothing
+
+  def total_price
+    if quantity.to_i > 3 && quantity.to_i < 5
+      (clothing.price.to_i * quantity.to_i) - 100
+    elsif quantity.to_i >= 5 && quantity.to_i > 3
+      (clothing.price.to_i * quantity.to_i) - 300
+    elsif quantity.to_i >= 5
+      (clothing.price.to_i * quantity.to_i) - 400
+    else
+      (clothing.price.to_i * quantity.to_i)
+    end
+  end
+  end
 end
