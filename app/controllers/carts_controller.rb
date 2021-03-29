21bb26748261caf68485b491239108e52dd0b041
JigmeT12 <jigmetashi02@gmail.com>
2021-03-30 02:57:22 +0600

    ADDED: validation to the cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,5 @@
 class CartsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
   before_action :set_cart, only: %i[ show edit update destroy ]
 
   # GET /carts or /carts.json
@@ -49,7 +50,8 @@ class CartsController < ApplicationController
 
   # DELETE /carts/1 or /carts/1.json
   def destroy
-    @cart.destroy
+    @cart.destroy if @cart .id == session[:cart_id]
+    session[:cart_id] = nil
     respond_to do |format|
       format.html { redirect_to carts_url, notice: "Cart was successfully destroyed." }
       format.json { head :no_content }
@@ -65,5 +67,10 @@ class CartsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def cart_params
       params.fetch(:cart, {})
+    end
+
+    def invalid_cart
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      redirect_to root_path, notice: "That cart doesn't exit"
     end
 end
