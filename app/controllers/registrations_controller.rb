bfee8fc585d6666a193929755c8a85bc84ec1765
JigmeT12 <jigmetashi02@gmail.com>
2021-03-30 19:40:37 +0600

    ADDED: profile pic

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,14 @@
 class RegistrationsController < Devise::RegistrationsController
 private
+    def index
+    @user = User.all
+    end 
+    
     def sign_up_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+        params.require(:user).permit(:name, :email, :password, :password_confirmation, :image)
     end
 
     def account_update_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+        params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password, :image)
     end
 end
