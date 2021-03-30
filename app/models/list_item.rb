32b9be356dc3170e48c3731fbd7dd32682a1b61d
JigmeT12 <jigmetashi02@gmail.com>
2021-03-30 23:05:32 +0600

    ADDED: H&M brand 30% off

--- app/models/list_item.rb
+++ app/models/list_item.rb
@@ -9,6 +9,8 @@ class ListItem < ApplicationRecord
       (clothing.price.to_i * quantity.to_i) - 300
     elsif quantity.to_i >= 7
       (clothing.price.to_i * quantity.to_i) - 400
+    elsif clothing.brand == "H&M"
+      (clothing.price.to_i * quantity.to_i) - (0.3 * (clothing.price.to_i * quantity.to_i))
     else
       (clothing.price.to_i * quantity.to_i)
     end
