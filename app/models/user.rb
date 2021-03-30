c5b81b1c8e74e62f2d1b562e96ed8e6772f9df0c
JigmeT12 <jigmetashi02@gmail.com>
2021-03-30 19:29:29 +0600

    ADDED: profile sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "200x200>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /image/
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :clothings
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_29_205131) do
+ActiveRecord::Schema.define(version: 2021_03_30_131510) do
 
   create_table "carts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -51,6 +51,10 @@ ActiveRecord::Schema.define(version: 2021_03_29_205131) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
